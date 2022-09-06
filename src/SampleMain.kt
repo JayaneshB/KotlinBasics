1eda837d19d036409241659bd5d1c6bddf737841
FI-User <fi-user@Fiusers-MacBook-Pro.local>
2022-09-06 17:54:38 +0530

    lets start the party

---
+++ src/SampleMain.kt
@@ -0,0 +1,36 @@
+fun main() {
+    /*val c=800000
+    println(c)
+    val a:String
+    val b:String
+    a="Hello World!"
+    b="Welcome"
+    println(a+" "+b)
+
+    var v1=123
+    val v2=v1.toDouble()
+    println(v2)*/
+
+
+    /*val a: Int = 100
+    val boxedA: Int? = a
+    val anotherBoxedA: Int? = a
+
+    val b:Int= 10000
+    val boxedB: Int? = b
+    val anotherBoxedB: Int? = b
+
+    println(boxedA === anotherBoxedA)
+    println(boxedB === anotherBoxedB)*/
+
+    val s="hello World!!!"
+    for ( i in s)
+        println(i)
+
+
+    val arr1=arrayOf(1,2,3,4,5)
+    for( i in arr1)
+        print(i)
+
+
+}
