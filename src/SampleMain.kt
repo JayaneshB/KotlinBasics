c3c187fd4449972a1f580f9baca4071a13d498ce
FI-User <fi-user@Fiusers-MacBook-Pro.local>
2022-09-07 12:00:17 +0530

    updated

--- src/SampleMain.kt
+++ src/SampleMain.kt
@@ -11,7 +11,6 @@ fun main() {
     val v2=v1.toDouble()
     println(v2)*/
 
-
     /*val a: Int = 100
     val boxedA: Int? = a
     val anotherBoxedA: Int? = a
@@ -23,14 +22,65 @@ fun main() {
     println(boxedA === anotherBoxedA)
     println(boxedB === anotherBoxedB)*/
 
-    val s="hello World!!!"
+    /*val s="hello World!!!"
     for ( i in s)
         println(i)
 
 
     val arr1=arrayOf(1,2,3,4,5)
     for( i in arr1)
-        print(i)
+        print(i)*/
+
+    //Array declaartion using array constructor
+    /*val arr1=Array(10,{i->i*1})
+    for (i in 1..arr1.size-1)
+        println(i)
+
+     */
+    //Array declaration using arrayof() function
+
+    //Implicit type declaration
+    /*
+    val arr1=arrayOf(1,2,3,4,5)
+    for (i in 0..arr1.size-1)
+        print(" "+arr1[i])
+    println()
+    */
+    //Explicit type declaration
+    /*val arr2= arrayOf<Int>(1,2,3,4,5,6,7,8,9)
+    val arr3=arrayOf<String>("Hello","world")
+    for (i in 0..arr2.size-1)
+        print(" "+arr2[i])
+    println()
+    for ( j in 0..arr3.size-1)
+        print(" "+arr3[j])
+    */
+
+    //Accessing and modifying array values
+
+    /*
+    val temp=arr1.get(3)
+    println(" "+temp)
+    arr1.set(1,12)
+    for (i in 0..arr1.size-1)
+        print(" "+arr1[i])
+    */
+
+    /*val str="Hello guys!,This is Jayanesh Balakrishnan"
+    print(str.subSequence(1,5))
+    */
+
+    /* If statement
+    val a=7;
+    val b=123;
+    val res=if(a>b) {
+        print("greater")
+    }
+    else {
+        print("smaller")
+    }
+    print(res)
+    */
 
 
 }
